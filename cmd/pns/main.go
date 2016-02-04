f8c89d2120248cbe7c0f5fd5d4851a5e602c63ae
Łukasz Pankowski <lukpank@o2.pl>
2016-02-04 08:38:28 +0100

    initial commit of pns

---
+++ cmd/pns/main.go
@@ -0,0 +1,124 @@
+// Copyright 2016 Łukasz Pankowski <lukpank at o2 dot pl>. All rights
+// reserved.  This source code is licensed under the terms of the MIT
+// license. See LICENSE file for details.
+
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	t, err := template.New("layout").Parse(layout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(os.Stdout, &Notes{"/test", notes})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Notes struct {
+	URL   string
+	Notes []*Note
+}
+
+func (n *Notes) TagURL(tag string) string {
+	tags := strings.Split(n.URL[1:], "/")
+	if strings.HasPrefix(tag, "/") {
+		tags[0] = tag
+		return strings.Join(tags, "/")
+	} else {
+		for _, t := range tags[1:] {
+			if tag == t {
+				return n.URL
+			}
+		}
+		return n.URL + "/" + tag
+	}
+}
+
+type Note struct {
+	Topics   []string
+	Tags     []string
+	Created  time.Time
+	Modified time.Time
+	ID       int
+	Text     string
+}
+
+var notes = []*Note{
+	{
+		Topics: []string{
+			"/test", "/test2",
+		},
+		Tags: []string{
+			"foo", "bar",
+		},
+		Created:  time.Now(),
+		Modified: time.Now(),
+		ID:       1,
+		Text:     "This is a **test**.",
+	},
+
+	{
+		Topics: []string{
+			"/test",
+		},
+		Tags: []string{
+			"foo", "baz",
+		},
+		Created:  time.Now(),
+		Modified: time.Now(),
+		ID:       2,
+		Text:     "This is another **test**.",
+	},
+}
+
+const layout = `
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="style.css">
+</head>
+
+<body>
+{{range $n := .Notes}}
+<div class="note">
+{{.Text}}
+
+<div class="note-footer">
+
+{{range .Topics}}
+<a href="{{$.TagURL .}}" class="tag">
+{{.}}
+</a>
+{{end}}
+
+{{range .Tags}}
+<a href="{{$.TagURL .}}" class="tag">
+{{.}}
+</a>
+{{end}}
+
+<span class="edit">
+<a href="/_/edit/{{.ID}}" class="tag">
+Edit
+</a>
+</span>
+
+<span class="time">
+{{.Modified.Format "2006-01-02 15:04:05 -0700"}}
+</span>
+
+</div>
+</div>
+{{end}}
+</body>
+</html>
+`
